afb0d421cc4d3581a0785041e10927b9457c78ec
Nurali <231087@astanait.edu.kz>
2024-02-21 18:36:01 +0600

    First init

---
+++ src/main/java/kz/aitu/springgranted/SpringGrantEdApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.springgranted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrantEdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrantEdApplication.class, args);
+    }
+
+}
