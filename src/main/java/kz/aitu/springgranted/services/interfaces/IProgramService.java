7a0d2de2d6ec728b6e9bfd16ddc548227494500a
maxsikxx <231324@astanait.edu.kz>
2024-03-01 23:29:20 +0500

    add method

--- src/main/java/kz/aitu/springgranted/services/interfaces/IProgramService.java
+++ src/main/java/kz/aitu/springgranted/services/interfaces/IProgramService.java
@@ -11,4 +11,6 @@ public interface IProgramService {
     List<Program> getProgramsForUser(String[] userElectiveNames, int userTotalScore);
     Program create(Program program);
     List<Program> createAll(List<Program> programs);
+
+    void deleteById(int id);
 }
